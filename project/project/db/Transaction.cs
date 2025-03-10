7b3620b5183fe2cfea2f619f774421e24ce2aec0
Phobos425 <asuhomlin2007@yandex.ru>
2025-03-10 19:57:20 +0300

    added db class

---
+++ project/project/db/Transaction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poject.db
+{
+    /// <summary>
+    /// класс, который 
+    /// </summary>
+    public class Transaction
+    {
+        public static int NewId { get; private set; } //количество транзакций
+
+        public int Id { get; private set; } // id транзакции
+        private DateTime _date; // дата транзакции
+        public DateTime Date {
+            get {
+                return _date;
+            }
+            set {
+                if (DateTime.Compare(value, DateTime.Now) <= 0) { // проверка, что такая дата существует
+                    _date = value;
+                } else
+                {
+                    throw new ArgumentException("Неверная дата");
+                }
+            }
+        }
+        public int ProdId { get; set; } // id товара
+        public string Name { get; set; } // Наименование товара
+        public int Count { get; set; } // Количество
+        public int PricePerUnit { get; set; } // Цена за единицу
+        private byte _region; // регион транзакции
+        public byte Region
+        {
+            get
+            {
+                return _region;
+            }
+            set
+            {
+                if (value >= 1 && value <= 89) // проверка существования региона
+                {
+                    _region = value;
+                } else
+                {
+                    throw new ArgumentException("Неверный регион");
+                }
+            }
+        }
+    }
+}
