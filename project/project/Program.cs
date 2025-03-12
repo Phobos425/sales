e30118c9dea05185d838c6714803ef764a59f731
Phobos425 <asuhomlin2007@yandex.ru>
2025-03-12 21:18:52 +0300

    ConsoleHandler added

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using project.db;
 
-public class Program
+public static class Program
 {
     public static void Main()
     {
-        List<Transaction> transactions = new List<Transaction>();
-        FileHandler.CurrentPath = @"..\..\..\db\db.csv";
-        FileHandler.DownloadData(ref transactions);
-        Console.WriteLine("1");
-        var trans = new Transaction(DateTime.Now, 3, "ada", 5, 15, 52);
-        Console.WriteLine(trans);
-        transactions.Add(trans);
-        FileHandler.WriteData(transactions);
+        
     }
 }
