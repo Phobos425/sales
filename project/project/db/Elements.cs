34a5ebc4dbd34999e9c51376012b25463a3be9ab
Phobos425 <asuhomlin2007@yandex.ru>
2025-03-13 21:54:56 +0300

    enum added

---
+++ project/project/db/Elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// элементы транзакции
+/// </summary>
+namespace project.db
+{
+    public enum Elements
+    {
+        Id,
+        Date,
+        ProdId,
+        Name,
+        Count,
+        PricePerUnit,
+        Region
+    }
+}
